a28ad3f289a7e3779918db006563865655a27262
miracleonyenma <miracleficient@gmail.com>
2023-08-11 00:48:17 +0100

    updates: add hybrid rendering config

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,10 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   devtools: { enabled: true },
+  routeRules: {
+    "/products/**": { ssr: true },
+    "/checkout/**": { ssr: false },
+  },
   css: ["~/assets/css/main.css", "vue-toastification/dist/index.css"],
   modules: ["@nuxtjs/tailwindcss"],
 });
